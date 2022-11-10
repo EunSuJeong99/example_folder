358912a4c9d347ce72fa95769ea1e42db2864777
DoJaeMin99 <116806638+DoJaeMin99@users.noreply.github.com>
2022-11-10 23:31:17 +0900

    챗봇안에 뭘 채움
    챗봇안에 뭘 채움

---
+++ chatbot/bot.py
@@ -0,0 +1,6 @@
+from models.intent.FeelModel import FeelModel
+from models.intent.IntentModel import IntentModel
+from models.intent.SituationModel import SituationModel
+from models.intent.WeatherModel import WeatherModel
+from models.intent.YNModel import YNModel
+from utils.Preprocess import Preprocess
